6125f38ec4cdb7808c490d79699332509bc21ce7
Henry Nguyen <henry.d.nguyen@uconn.edu>
2019-06-06 18:52:49 -0400

    Adding CNAME

---
+++ index.js
@@ -0,0 +1,12 @@
+var toggleButton = document.getElementById('toggleSlider'),
+    slide = document.querySelector('.slide');
+
+toggleButton.addEventListener('click', toggleSlider, false);
+
+function toggleSlider() {
+  if (slide.classList.contains('slide-up')) {
+    slide.classList.remove('slide-up');
+  } else {
+    slide.classList.add('slide-up');
+  }
+}
